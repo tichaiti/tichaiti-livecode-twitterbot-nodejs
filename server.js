4fda87dfcca93ecf546d99241697ddc5b1c41931
accimeesterlin <accimeesterlin@yahoo.com>
2021-01-30 17:18:34 -0500

    adding twitter post

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
+require('dotenv').config();
 var Twitter = require('twitter');
  
 var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_SECRET
 });
  
-var params = {screen_name: 'nodejs'};
+var params = {screen_name: 'BarackObama'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-    console.log(tweets);
+    console.log(tweets[0]);
   } else {
       console.log('Error: ', error);
   }
+});
+
+client.post('statuses/update', {status: 'Tichaiti - Live Twitter Example - Ignore this tweet'},  function(error, tweet, response) {
+    if(error) throw error;
+    console.log(tweet);  // Tweet body.
+    console.log(response);  // Raw response object.
 });
