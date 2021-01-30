04c598a86fb0a456bf6e2c9709f326b6c1417c1a
accimeesterlin <accimeesterlin@yahoo.com>
2021-01-30 16:50:51 -0500

    adding log for errors

--- server.js
+++ server.js
@@ -11,5 +11,7 @@ var params = {screen_name: 'nodejs'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     console.log(tweets);
+  } else {
+      console.log('Error: ', error);
   }
 });
